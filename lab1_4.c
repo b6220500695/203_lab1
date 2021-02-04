6bff18975d5eaafaf82b5d63044290885d784135
mmint.m <pheeraya.p@ku.th>
2021-02-04 17:28:15 +0700

    2

--- lab1_4.c
+++ lab1_4.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-#include <Math.h>
 int main()
 {
     int s,x1,x2;
     scanf ("%d%d",&x1,&s);
     //S=(X1+X2)/2;//
-    if (s>-1000<x1||s<1000<x1)
+    if (s>-1000<x1||s<1000>x1)
     {
        x2=(s*2)-x1;
     }
