fd1853b8dd8f03ac64fe00ac49e26bc69847128a
mmint.m <pheeraya.p@ku.th>
2021-02-04 15:50:13 +0700

    ok

--- lab1_2.c
+++ lab1_2.c
@@ -3,10 +3,10 @@ int main()
 {
    int a,b,c;
 
-   if(0<=a<=squrt(10,9));
-   scanf("%d",a);
-   if(0<=b<=squrt(10,9));
-   scanf("%d",b);
+   if(0<=a<=pow(10,9));
+   scanf("%d",&a);
+   if(0<=b<=pow(10,9));
+   scanf("%d",&b);
    c=a+b;
    printf("%d",c);
     return 0;
