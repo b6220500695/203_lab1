1717210bee87a3b47c53ed9766c1fcfe3e253ceb
mmint.m <pheeraya.p@ku.th>
2021-02-04 17:09:49 +0700

    ok

--- lab1_3.c
+++ lab1_3.c
@@ -1,13 +1,12 @@
-#include <stdio.h>
-int main(){
-    int S,X1,X2;
-    printf("Plese insert X1 and S\n");
-    scanf ("%d%d",&X1,&S);
-    if (S>-1000<X1 || S<1000<X1)
-    {
-       X2 = (S*2)-X1;
-    printf("X2 = %d",X2);
-    }
-    else
-   printf("Error!! Please insert new value");
+#include<stdio.h>
+int main()
+{
+   long int n,x,y;
+   scanf("%ld",&n);
+   if(n>=0)
+   {
+      x=n%3;
+      y=n%11;
+   }
+   printf("%ld %ld",x,y);
 }
